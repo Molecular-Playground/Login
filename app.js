77248517c8fce2bc363af67bd88ee5e084328bea
Ethan Fine <ethanfine@umass.edu>
2016-02-16 22:11:53 -0500

    basic functionality minus some internal dependancies

--- app.js
+++ app.js
@@ -4,16 +4,55 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var bcrypt = require('bcrypt-nodejs');
+var njwt = require('njwt');
+var fs = require('fs');
+var db = require('./lib/db');
 
 var app = express();
+
+//Non Synchronous call is on purpose. Config must be parse before we continue.
+var config = JSON.parse(fs.readFileSync('config.json','utf8'));
+var secret = config.secret;
+var signkey = config.signkey;
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', routes);
+//Login post function goes here.
+app.post('/',funtion(req,res,next){
+  var username = req.body.username;
+  var password = req.body.parser;
+  db.connect("SELECT * from users where username = $(username)",funtion(err,results){
+    if(err){
+      console.error("error connecting to database");
+      next(new Error('DB error'));
+    } else{
+      bcrypt.compare(password,results.rows[0].password,function(err,success){
+        if(err){
+          console.error("bcrypt error");
+          next(new Error('bcrpyt error'));
+        } else{
+          if(success){
+            var claims = {
+              iss: "Molecular Playground URL",
+              sub: "$(results.rows[0].id)",
+              username: username,
+              admin: "$(results.rows[0].admin)"
+            };
+            var jwt = njwt.create(claims,signkey);
+            var token = jwt.compact;
+            res.send({
+              token: token
+            });
+          }
+        }
+      })
+    }
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
